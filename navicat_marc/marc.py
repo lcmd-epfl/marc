346c1df4f11f94dddb6db573b21e89f5129366ba
Dennis Svatunek <39262379+dsvatunek@users.noreply.github.com>
2024-07-15 21:42:23 +0200

    Fix format string calculation to ensure correct padding width
    Calculation of necessary digits in format_string could result in floats and thus strings like "02.0". Additionally formatting with zero padding should be "02d" and not "02".

--- navicat_marc/marc.py
+++ navicat_marc/marc.py
@@ -243,7 +243,7 @@ def run_marc():
     # Output name generation
     outnames = [molecule.name for molecule in molecules]
     if None in outnames:
-        format_string = f"0{min(np.floor(l / 10), 1) + 1}"
+        format_string = f"0{int(round(min(np.floor(l / 10), 1) + 1))}d"
         outnames = [f"{basename}_{idx:{format_string}}" for idx in range(l)]
 
     # Plot tsne
